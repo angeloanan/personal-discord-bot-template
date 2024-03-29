8bea6f7071265d92df7a8d23b605ddcc864e05f7
Christopher Angelo <angelo@angeloanan.xyz>
2024-03-29 23:05:51 +0700

    fix(drizzle): add out dir

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 // biome-ignore lint/style/noDefaultExport: Config file
 export default defineConfig({
 	schema: './db/schema.ts',
+	out: './drizzle',
 	driver: 'better-sqlite',
 	verbose: true,
 	strict: true
