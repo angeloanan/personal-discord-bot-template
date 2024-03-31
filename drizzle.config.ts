12bdefe354fb3aa8c69def83b9349a3a1851c6e0
Christopher Angelo <angelo@angeloanan.xyz>
2024-03-31 19:44:38 +0700

    fix: update drizzle-kit config to include db creds

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'drizzle-kit'
 
 // biome-ignore lint/style/noDefaultExport: Config file
 export default defineConfig({
-	schema: './db/schema.ts',
-	out: './drizzle',
-	driver: 'better-sqlite',
+	schema: 'src/db/schema.ts',
+	out: 'drizzle',
+	driver: 'libsql',
+	dbCredentials: {
+		url: process.env.DATABASE_URL ?? 'file:database.db',
+	},
 	verbose: true,
 	strict: true
 })
